7290bb4cad8b1eabe1d625370d4080737a203625
eva <evakazakovskaia@gmail.com>
2022-02-02 13:52:12 +0200

    added new art and shooting

---
+++ Assets/Bullet.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float speed = 20f;
+    public Rigidbody2D rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.velocity = transform.right * speed;
+    }
+
+
+    void OnBecomeInvisible()
+    {
+        //Destroy();
+    }
+}
