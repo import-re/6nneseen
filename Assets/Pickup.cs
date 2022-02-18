7f2efdcf1ecafb05c624cf340fda006ca4a46cfb
eva <evakazakovskaia@gmail.com>
2022-02-18 14:58:14 +0200

    added weapon pickup system

--- Assets/Pickup.cs
+++ Assets/Pickup.cs
@@ -4,19 +4,33 @@ using UnityEngine;
 
 public class Pickup : MonoBehaviour
 {
+    public Vector3 playerPosition;
+    public Vector3 weaponPosition;
+    public GameObject player;
+    public GameObject relv;
+    // Transform PlayerAttachment;
+    //public Transform WeaponAttachment;
+
+    private bool weaponHit = false;
+
+
+    void Update()
+    {
+        if (weaponHit && Input.GetKeyDown(KeyCode.E))
+        {
+            Debug.Log("E is pressed");
+            PickUp();
+        }
+
+    }
+
+
     void OnCollisionEnter2D(Collision2D coll)
     {
         if (coll.gameObject.tag == "Player")
         {
-            if (Input.GetKeyDown(KeyCode.E))
-            {
-                Debug.Log("E is pressed");
-                PickUp();
-            }
-            else
-            {
-                Debug.Log("Please press E to pick up the weapon.");
-            }
+            Debug.Log("Please press E to pick up the weapon.");
+            weaponHit = true;
         }
 
     }
@@ -25,5 +39,7 @@ public class Pickup : MonoBehaviour
     void PickUp()
     {
         Debug.Log("yes");
+        relv.transform.SetParent(player.transform);
+        //PlayerAttachment.transform.SetParent(WeaponAttachment.transform);
     }
 }
