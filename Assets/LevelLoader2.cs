addabc1619d4dfd55acdff4746c601c62fb9894f
evodka <lexaeva12@gmail.com>
2022-08-23 17:52:16 +0300

    added aportal and second scene loading

---
+++ Assets/LevelLoader2.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader2 : MonoBehaviour
+{
+    public string sceneToLoad;
+
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        if (coll.gameObject.tag == "Player")
+        {
+            SceneManager.LoadScene(sceneToLoad);
+            DontDestroyOnLoad(coll.gameObject);
+        }
+
+    }
+
+
+
+}
