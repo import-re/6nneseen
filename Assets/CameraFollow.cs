e17ec793ae27094588f9bac43272f65cb9a3f8d2
evodka <lexaeva12@gmail.com>
2022-09-22 23:58:21 +0300

    camera follows the player

---
+++ Assets/CameraFollow.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class CameraFollow : MonoBehaviour
+{
+    public float FollowSpeed = 2f;
+    public float yOffset =1f;
+    public Transform target;
+    //public Transform target;
+
+
+    void Start()
+    {
+        target = GameObject.Find("Player").transform;
+    }
+
+
+    void Update()
+    {
+        Vector3 newPos = new Vector3(target.position.x,target.position.y + yOffset,-10f);
+        transform.position = Vector3.Slerp(transform.position,newPos,FollowSpeed*Time.deltaTime);
+    }
+}
