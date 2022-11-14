4f27db39ef0dba97b9afcc82fb4ab3ee4c9d0865
eva <lexaeva12@gmail.com>
2022-11-14 16:47:39 +0200

    fixed bugs

--- Assets/DropHeath.cs
+++ Assets/DropHeath.cs
@@ -5,21 +5,24 @@ public class DropHeath : MonoBehaviour
 {
     public GameObject healthBooster;
     public Transform HealSpawnPoint;
+    private bool hasChecked; 
 
-    void Start()
+    void Update()
     {
         HealSpawnPoint = GameObject.Find("HealSpawnPoint").transform;
         Scene currentScene = SceneManager.GetActiveScene ();
         string sceneName = currentScene.name;
-        if (sceneName == "BossScene")
+        if (sceneName == "BossScene" & !hasChecked)
         {
 
             InvokeRepeating("Spawn", 10f, 10f);
+            hasChecked = true;
         }
     }
 
     void Spawn()
     {
         Instantiate(healthBooster, HealSpawnPoint.position, HealSpawnPoint.rotation);
+        Debug.Log("Instantiating health booster");
     }
 }
