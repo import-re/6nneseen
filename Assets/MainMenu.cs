6b29a3546f827fc96b7fcd4513dc6607954dab8f
evodka <lexaeva12@gmail.com>
2022-11-14 21:22:45 +0200

    added cartoon, animation

--- Assets/MainMenu.cs
+++ Assets/MainMenu.cs
@@ -9,15 +9,69 @@ public class MainMenu : MonoBehaviour
     public AudioSource startMusic;
     public AudioSource opitonMusic;
     public GameObject startMenu;
+    int timesPlayed;
+    public GameObject cartoon;
+    public AudioSource cartoonMusc;
+    private float TimeTillTransition = 15;
+    float timeTillStart = 50;
+    public GameObject TrippyAnimation;
+    bool hasChecked;
+    bool hasCheckedAnim;
+
 
 
     void Start()
     {
+        timesPlayed = PlayerPrefs.GetInt("TimesPlayed", 0);
+        timesPlayed++;
+        PlayerPrefs.SetInt("TimesPlayed", timesPlayed);
         buttonSound = GetComponent<AudioSource>();
         exitSound = GetComponent<AudioSource>();
         optionsMenu.SetActive(false);
+        TrippyAnimation.SetActive(false);
         loadMainPage();
+        if (timesPlayed >= 2)
+        {
+            loadMainPage();
+        }
+        else
+        {
+            cartoon.SetActive(true);
+            cartoonMusc.Play();
+            startMusic.Pause();
+            opitonMusic.Pause();
+        }
 
+    }
+
+    void Update()
+    {
+        TimeTillTransition -= Time.deltaTime;
+        timeTillStart -= Time.deltaTime;
+        if (TimeTillTransition < 0 & !hasCheckedAnim)
+        {
+            TrippyAnimation.SetActive(true);
+            hasCheckedAnim = true;
+        }
+        if (timeTillStart < 0 & !hasChecked)
+        {
+            loadMainPage();
+            hasChecked = true;
+
+        }
+    }
+
+
+    void LoadCartoon()
+    {
+        cartoon.SetActive(true);
+        cartoonMusc.Play();
+        startMusic.Pause();
+        opitonMusic.Pause();
+        if (TimeTillTransition < 0)
+        {
+            TrippyAnimation.SetActive(true);
+        }
     }
 
 
@@ -37,14 +91,19 @@ public class MainMenu : MonoBehaviour
 
     public void loadMainPage()
     {
+        cartoonMusc.Stop();
+        cartoon.SetActive(false);
+        TrippyAnimation.SetActive(false);
         opitonMusic.Stop();
         startMenu.SetActive(true);
         startMusic.Play();
+        cartoonMusc.Pause();
     }
 
     public void loadOptions()
     {
         startMusic.Stop();
+        cartoonMusc.Pause();
         buttonSound.Play();
         optionsMenu.SetActive(true);
         opitonMusic.Play();
